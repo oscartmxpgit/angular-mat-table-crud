133e7dae9be0a1f34558daef417827c2c5e0460d
oscartmxpgit <92880098+oscartmxpgit@users.noreply.github.com>
2023-03-13 07:54:26 +0100

    delete btn

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { DataService } from './services/data.service';
 
 @Component({
   selector: 'app-root',
@@ -9,4 +10,16 @@ import { Component, OnInit } from '@angular/core';
 export class AppComponent implements OnInit {
   ngOnInit(): void {
   }
+
+  constructor(
+    public dataService: DataService) {
+  }
+
+  DeleteAll() {
+    if(confirm("¿Seguro que desea eliminar todo?")) {
+      this.dataService.deleteAll();
+      window.location.reload();
+    }
+  }
+
 }
