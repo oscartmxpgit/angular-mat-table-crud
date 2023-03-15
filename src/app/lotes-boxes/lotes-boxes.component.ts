675c7dc7deefedaac3ba7fcaadda5cf94f41445f
oscartmxpgit <92880098+oscartmxpgit@users.noreply.github.com>
2023-03-15 18:54:08 +0100

    nuevo lote dialog

--- src/app/lotes-boxes/lotes-boxes.component.ts
+++ src/app/lotes-boxes/lotes-boxes.component.ts
@@ -5,8 +5,11 @@ import { MatDialog } from '@angular/material/dialog';
 import { MatPaginator } from '@angular/material/paginator';
 import { MatSort } from '@angular/material/sort';
 import { MatTableDataSource } from '@angular/material/table';
+import { MatTabGroup } from '@angular/material/tabs';
 import { Router } from '@angular/router';
+import { LoteInfoComponent } from 'app/dialogs/lote-info/lote-info.component';
 import { DataService } from 'app/services/data.service';
+import { LotesDataService } from 'app/services/lotes-data.service';
 import { TableContentComponent } from 'app/table-content/table-content.component';
 import { ResetDialogComponent } from '../dialogs/reset-dialog/reset-dialog.component';
 
@@ -16,19 +19,19 @@ import { ResetDialogComponent } from '../dialogs/reset-dialog/reset-dialog.compo
   styleUrls: ['./lotes-boxes.component.scss']
 })
 export class LotesBoxesComponent implements OnInit {
-  constructor(private router: Router, public dialog: MatDialog, public dataService: DataService, public httpClient: HttpClient,) { }
+  constructor(private router: Router, public dialog: MatDialog, public dataService: DataService, public httpClient: HttpClient, public lotesDataService: LotesDataService) { }
   obs: any = [];
   panelOpenState = false;
 
   @ViewChild(MatPaginator) paginator: MatPaginator;
   @ViewChild(MatSort) sort: MatSort;
+  @ViewChild(MatTabGroup) tabGroup: MatTabGroup;
 
   maxLote = 0;
   lotesDatabase: DataService | null;
 
   ngOnInit() {
-    this.dataService.getNumLotes();
-    this.maxLote = this.dataService.numLotes;
+    this.maxLote = this.lotesDataService.getNumLotes();
 
     for (let index = 0; index < this.maxLote; index++) {
       this.tabs.push('Lote ' + (index + 1))
@@ -37,7 +40,7 @@ export class LotesBoxesComponent implements OnInit {
 
   refresh() {
     //this.loadData();
-    if(confirm("¿Estás seguro de eliminar todos los elementos? ")) {
+    if (confirm("¿Estás seguro de eliminar todos los elementos? ")) {
       window.location.reload();
     }
 
@@ -47,9 +50,17 @@ export class LotesBoxesComponent implements OnInit {
   selected = new FormControl(0);
 
   addTab() {
-    this.maxLote += 1;
-    this.tabs.push('Lote ' + (this.maxLote));
-    this.selected.setValue(this.tabs.length - 1);
+    const dialogRef = this.dialog.open(LoteInfoComponent, {
+      data: { loteId: this.maxLote + 1 }, width: '85%', panelClass: 'custom-dialog-container'
+    });
+
+    dialogRef.afterClosed().subscribe(result => {
+      if (result === 1) {
+        this.maxLote += 1;
+        this.tabs.push('Lote ' + (this.maxLote));
+        this.selected.setValue(this.tabs.length - 1);
+      }
+    });
   }
 
   removeTab(index: number) {
